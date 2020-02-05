ce7412266d4eaecb1cc411f2754f9da1062b3011
noelpalo <noel.palo@vokke.com.au>
2020-02-05 15:26:58 +0800

    Vokke Training - Topic 16, Add Custom datatype

---
+++ app/VokkeTraining/Classes/Custom/DataTypes/PointType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\VokkeTraining\Classes\Custom\DataTypes;
+
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+
+use App\VokkeTraining\Classes\Custom\ValueObjects\Point;
+
+
+/**
+ * My custom datatype.
+ */
+class PointType extends Type
+{
+    const POINT = 'point';
+
+    public function getName()
+    {
+        return self::POINT;
+    }
+
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return 'POINT';
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        list($longitude, $latitude) = sscanf($value, 'POINT(%f %f)');
+
+        return new Point($latitude, $longitude);
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if ($value instanceof Point) {
+            $value = sprintf('POINT(%F %F)', $value->getLongitude(), $value->getLatitude());
+        }
+
+        return $value;
+    }
+
+    public function canRequireSQLConversion()
+    {
+        return true;
+    }
+
+    public function convertToPHPValueSQL($sqlExpr, AbstractPlatform $platform)
+    {
+        return sprintf('AsText(%s)', $sqlExpr);
+    }
+
+    public function convertToDatabaseValueSQL($sqlExpr, AbstractPlatform $platform)
+    {
+        return sprintf('PointFromText(%s)', $sqlExpr);
+    }
+}
